16aa14e6ecf965d13094ffc0e429510df0280a13
Surya Nelakanti <suryateja.nelakanti@gmail.com>
2025-06-20 15:45:18 +0530

    removed unrequired files

--- jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest/presets/js-with-ts-esm',
-  testEnvironment: 'node',
-  testMatch: ['**/?(*.)+(spec|test).ts'],
-  roots: ['<rootDir>/backend'],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-};
